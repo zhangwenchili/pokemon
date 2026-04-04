9ccaa7ac5adc02ccbef359ff9bc337b94ff370a0
Zhangwenchi Li <whitegive92@gmail.com>
2026-04-04 16:16:13 -0400

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,54 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+
+require "net/http"
+require "json"
+
+puts "Clearing old data..."
+SpeciesTypeMap.destroy_all
+Type.destroy_all
+Species.destroy_all
+
+(1..386).each do |id|
+  puts "Fetching species ##{id}..."
+
+  species_url = URI("https://pokeapi.co/api/v2/pokemon-species/#{id}/")
+  pokemon_url = URI("https://pokeapi.co/api/v2/pokemon/#{id}/")
+
+  species_response = JSON.parse(Net::HTTP.get(species_url))
+  pokemon_response = JSON.parse(Net::HTTP.get(pokemon_url))
+
+  # name
+  english_name =
+    species_response["names"]
+      .find { |n| n.dig("language", "name") == "en" }
+      &.dig("name") || species_response["name"]
+
+  # image
+  image_url =
+    pokemon_response.dig("sprites", "other", "official-artwork", "front_default") ||
+    pokemon_response.dig("sprites", "front_default")
+
+  # create species
+  species = Species.create!(
+    name: english_name,
+    pokedex_number: id,
+    image_url: image_url
+  )
+
+  # handle types
+  pokemon_response["types"].each do |type_entry|
+    type_name = type_entry.dig("type", "name")
+
+    type = Type.find_or_create_by!(name: type_name)
+
+    SpeciesTypeMap.create!(
+      species: species,
+      type: type
+    )
+  end
+end
+
+puts "Done! Seeded species, types, and relationships."
